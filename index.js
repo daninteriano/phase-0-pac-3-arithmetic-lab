e48a4ab8ab30ae002424951be33be84206c602a2
Daniel Interiano <dannyboyint@gmail.com>
2021-06-26 15:34:46 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(num1, num2){
+    let sum = num1 + num2
+    return sum
+}
+function subtract(sub1, sub2){
+    let total = sub1 - sub2
+    return total
+}
+function multiply(mult1, mult2){
+    let star = mult1 * mult2
+    return star
+}
+function divide(div1, div2){
+    let bar = div1 / div2
+    return bar
+}
+add(1, 585);
+subtract(187,1);
+multiply(232212, 2);
+divide();
+
+function increment(haki){
+    return ++haki;
+}
+function decrement(logia){
+    return --logia;
+}
+function makeInt(str){
+    return parseInt(str, 10)
+}
+function preserveDecimal(tri){
+    return parseFloat(tri)
+}
+increment(8);
+decrement(7);
+makeInt('happy');
+preserveDecimal('80.23435');
